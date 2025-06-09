cb69a9fba68fb6b7c831dbb8a860552c4d4849af
21089jp <21089jp@hvhs.school.nz>
2025-06-09 22:48:31 +0000

    Implemented a bunch of information, mostly placeholder. Ready for feedback on my initial design.

--- script.js
+++ script.js
@@ -3,3 +3,5 @@
 * JavaScript for one-page CV website
 * Made by Jayden Pressley
 ***********************************************/
+
+console.log('Script is running...\nEnable smooth scrolling in "chrome://flags"')
