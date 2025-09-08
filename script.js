cab0eadf768afcdc9acba045bbd84aab9851f23d
21089jp <21089jp@hvhs.school.nz>
2025-09-09 10:53:20 +1200

    Formatted all of the code.

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 /***********************************************
-* script.js
-* JavaScript for one-page CV website
-* Made by Jayden Pressley
-***********************************************/
+ * script.js
+ * JavaScript for one-page CV website
+ * Made by Jayden Pressley
+ ***********************************************/
 
-console.log('script.js is running...\nEnable smooth scrolling in "chrome://flags"');
+console.log(
+  'script.js is running...\nEnable smooth scrolling in "chrome://flags"'
+);
 
+/***********************************************
+ * copyEmail
+ * Copies text to clipboard, console logs it
+ ***********************************************/
 function copyEmail() {
-  const email = 'pressleyjayden7@gmail.com';
-  navigator.clipboard.writeText(email).then(() => console.log('Email copied.'));
-  console.log(email)
+  const email = "pressleyjayden7@gmail.com";
+  navigator.clipboard.writeText(email).then(() => console.log("Email copied."));
+  console.log(email);
 }
 
- function downloadCV() {
-    console.log('Downloading CV');
-  const link = document.createElement('a');
-  link.href = 'images/JaydenPressleyMiniCV.pdf';  
-  link.download = 'JaydenPressleyCV';
+/***********************************************
+ * donwloadCV
+ * Takes file from images folder
+ * Downloads it via link
+ ***********************************************/
+function downloadCV() {
+  console.log("Downloading CV");
+  const link = document.createElement("a");
+  link.href = "images/JaydenPressleyMiniCV.pdf";
+  link.download = "JaydenPressleyCV";
   link.click();
-  
-  console.log(link.download)
-}
 
+  console.log(link.download);
+}
